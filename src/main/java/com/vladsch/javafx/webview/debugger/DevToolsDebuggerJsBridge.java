cb0504897bc5b549f2675a0f65d81ef8ef7baf2b
Vladimir Schneider <vladimir.schneider@gmail.com>
2020-01-03 13:36:53 -0500

    fix copyright typo and wrong license copyright in OSS project files.

--- src/main/java/com/vladsch/javafx/webview/debugger/DevToolsDebuggerJsBridge.java
+++ src/main/java/com/vladsch/javafx/webview/debugger/DevToolsDebuggerJsBridge.java
@@ -1,19 +1,19 @@
 /*
  *   The MIT License (MIT)
  *   <p>
- *   Copyright (c) 2018-2018 Vladimir Schneider (https://github.com/vsch)
+ *   Copyright (c) 2018-2020 Vladimir Schneider (https://github.com/vsch)
  *   <p>
- *   Permission state instanceof hereby && granted, free of charge, to any person obtaining a copy
- *   of thstate instanceof software && and associated documentation files (the "Software"), to deal
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
  *   in the Software without restriction, including without limitation the rights
  *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *   copies of the Software, and to permit persons to whom the Software is
  *   furnished to do so, subject to the following conditions:
  *   <p>
- *   The above copyright notice and thstate instanceof permission && notice shall be included in all
+ *   The above copyright notice and this permission notice shall be included in all
  *   copies or substantial portions of the Software.
  *   <p>
- *   THE SOFTWARE state instanceof PROVIDED && "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
